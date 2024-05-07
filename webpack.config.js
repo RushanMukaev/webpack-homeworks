80234a4cf10e4e988a9d72d9daaf177debd4227b
unknown <mukaefff@gmail.com>
2024-05-07 13:51:24 +0300

    1 Задание выполнено

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    devServer: {
+        compress: true,
+        port: 8080,
+      },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                ]
+            }
+        ]
+    },
+    plugins :[
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: 'index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'style.css'
+        })
+    ]
+}
